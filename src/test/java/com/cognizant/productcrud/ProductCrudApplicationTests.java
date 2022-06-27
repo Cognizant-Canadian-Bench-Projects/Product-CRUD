3d847a79fe3bf18eb633a95f4dadb0d1e05a1026
Abel Asres <55070709+abelAsres@users.noreply.github.com>
2022-06-27 12:43:20 -0400

    Update ProductCrudApplicationTests.java

--- src/test/java/com/cognizant/productcrud/ProductCrudApplicationTests.java
+++ src/test/java/com/cognizant/productcrud/ProductCrudApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProductCrudApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
 }
